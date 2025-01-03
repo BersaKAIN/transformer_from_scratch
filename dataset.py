bced29912822ed143a12ac53302e740c313abee1
BersaKAIN <bersakain@gmail.com>
2025-01-02 18:19:50 -0800

    Update dataset.py to include Any

--- dataset.py
+++ dataset.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
+from typing import Any
+
 class BilingualDataset(Dataset):
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
         super().__init__()
